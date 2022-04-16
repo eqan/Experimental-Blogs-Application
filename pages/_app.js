cc8314ff3371b7a53a6873d2792d22de3741df39
eqan <eqan.ahmad123@gmail.com>
2022-04-16 12:10:37 +0500

    Basic Blog Website Complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Header from '../components/header'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <main className='container'>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
 
 export default MyApp
